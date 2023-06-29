bd0a406eac49b990bce9deb26394e70f3304cf28
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2023-06-29 05:48:23 -0600

    refactor: more changes to round out the projects section to be a bit more efficient

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,42 @@
+import Rhymepage from "../assets/rhymepage.png";
 import Rhymebook from "../assets/rhymebook.png";
 import VaultCounter from "../assets/vault-counter.png";
-import AOLogo from "../assets/ao-logo.png";
+// import AOLogo from "../assets/ao-logo.png";
 import Project from "./Project";
+
+const ProjectData = [
+  {
+    image: Rhymepage,
+    name: "Rhymepage",
+    desc: "Version 2.0 of Rhymebook, a lyric writing application that writes your bars with you! Rebuilt with Vite, TailwindCSS, and DaisyUI",
+    live: "https://rhymepage.com",
+    code: "https://github.com/Arrangedgodly/rhymebook-vite"
+  },
+  {
+    image: Rhymebook,
+    name: "Rhymebook",
+    desc: "The original iteration of an API powered lyric writing application that writes your bars with you!",
+    live: "https://rhymebook.graydonwasil.com/",
+    code: "https://github.com/Arrangedgodly/rhymebook-chakra",
+  },
+  {
+    image: VaultCounter,
+    name: "Vault Counter",
+    desc: "An application to make counting vaulted cash a breeze.",
+    live: "https://vault.graydonwasil.com/",
+    code: "https://github.com/Arrangedgodly/vault-vite",
+  }
+];
+
+/*
+{
+    image: AOLogo,
+    name: "ArtOffishal",
+    desc: "An art gallery and merch shop for ArtOffishal",
+    live: "https://artoffishal.com/",
+    code: "https://github.com/orgs/ArtofFish/repositories",
+  },
+*/
 
 const Projects = () => {
   return (
@@ -10,69 +45,41 @@ const Projects = () => {
       id="projects"
     >
       <div className="carousel w-full max-w-[95vw]">
-        <div
-          id="slide1"
-          className="carousel-item relative w-full items-center justify-center h-screen"
-        >
-          <Project
-            image={Rhymebook}
-            name="Rhymebook"
-            desc="An API powered lyric writing application that writes your bars with you!"
-            live="https://rhymebook.graydonwasil.com/"
-            code="https://github.com/Arrangedgodly/rhymebook-chakra"
-          />
-          <div
-            className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-            data-aos="fade-up"
-          >
-            <a href="#slide3" className="btn btn-circle btn-nuetral drop-shadow-xl">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle btn-nuetral drop-shadow-xl">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div
-          id="slide2"
-          className="carousel-item relative w-full items-center justify-center h-screen"
-        >
-          <Project
-            image={VaultCounter}
-            name="Vault Counter"
-            desc="An application to make counting vaulted cash a breeze."
-            live="https://vault.graydonwasil.com/"
-            code="https://github.com/Arrangedgodly/vault-vite"
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle btn-nuetral drop-shadow-xl">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle btn-nuetral drop-shadow-xl">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div
-          id="slide3"
-          className="carousel-item relative w-full items-center justify-center h-screen"
-        >
-          <Project
-            image={AOLogo}
-            name="ArtOffishal"
-            desc="An art gallery and merch shop for ArtOffishal"
-            live="https://artoffishal.com/"
-            code="https://github.com/orgs/ArtofFish/repositories"
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle btn-nuetral drop-shadow-xl">
-              ❮
-            </a>
-            <a href="#slide1" className="btn btn-circle btn-nuetral drop-shadow-xl">
-              ❯
-            </a>
-          </div>
-        </div>
+        {ProjectData.map((project, index) => {
+          const previousIndex =
+            index !== 0 ? index - 1 : ProjectData.length - 1;
+          const nextIndex = index !== ProjectData.length - 1 ? index + 1 : 0;
+
+          return (
+            <div
+              key={`Project-${project.name}`}
+              id={`slide${index + 1}`}
+              className="carousel-item relative w-full items-center justify-center h-screen"
+            >
+              <Project
+                image={project.image}
+                name={project.name}
+                desc={project.desc}
+                live={project.live}
+                code={project.code}
+              />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a
+                  href={`#slide${previousIndex + 1}`}
+                  className="btn btn-circle btn-nuetral drop-shadow-xl"
+                >
+                  {`<`}
+                </a>
+                <a
+                  href={`#slide${nextIndex + 1}`}
+                  className="btn btn-circle btn-nuetral drop-shadow-xl"
+                >
+                  {`>`}
+                </a>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
